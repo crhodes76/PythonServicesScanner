b5d56c065877f93aa88f410f2bd4c7db894487b0
Charles Rhodes <crhodes76@users.noreply.github.com>
2025-02-13 09:27:00 -0500

    if the service is stopped attempt to restart the service if a exception occurs log the exception

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import psutil
-
+import subprocess
 def get_service(service_name):
     for service in psutil.win_service_iter():
         if service.name().lower() == service_name.lower():
@@ -12,5 +12,14 @@ if __name__ == "__main__":
         the_service = get_service(service)
         if the_service:
             print(f"{the_service.display_name()} is {the_service.status()}")
+            if the_service.status() == "stopped":
+                print('Attempting to start the service ' + the_service.display_name())
+                try:  
+                    result = subprocess.run(["net", "start", the_service.name()], capture_output=True, text=True, check=True)
+                except Exception as e:
+                    print(f"An exception as occurred {e}")
+                finally:
+                    print(f"{the_service.display_name()} has been started")
+                    print(f"{the_service.display_name()} is {the_service.status()}")
         else:
             print(f"{service} not found")
